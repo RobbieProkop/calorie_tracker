64c684f0f0679aa69ff946feaf027ebd6bda12c2
Robbie Prokop <robbieprokop@gmail.com>
2022-11-01 10:32:07 +0400

    create iife ctrls

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// main app.js
+//Storeage Controller
+
+// Item Controller
+
+const ItemCtrl = (function () {
+  console.log("iife :>> ");
+})();
+
+// UI Controller
+
+const UICtrl = (function () {
+  console.log("iife : UI ");
+})();
+
+// App Controller
+const App = (function (ItemCtrl, UICtrl) {})(ItemCtrl, UICtrl);
